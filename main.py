71c9002dbe2dc5fc113b80cae38f57d6a8e6f2f9
Ediz <ediz.tefenlilioglu@gmail.com>
2023-10-02 15:08:36 +0200

    fix proxy and add variable impersonate

--- main.py
+++ main.py
@@ -11,43 +11,98 @@ class SimpleSession:
     def __init__(self):
         self.session = requests.Session()
 
-    def get(self, url: str, headers: Optional[Dict[str, str]] = None, impersonate: Optional[str] = "chrome110", **kwargs):
-        return self.session.get(str(url), headers=headers, impersonate=impersonate, **kwargs)
+    def get(self, url: str, headers: Optional[Dict[str, str]] = None, **kwargs):
+        return self.session.get(str(url), headers=headers, **kwargs)
 
-    def post(self, url: str, json: Optional[Dict[str, Any]] = None, headers: Optional[Dict[str, str]] = None, impersonate: Optional[str] = "chrome110", **kwargs):
-        return self.session.post(str(url), headers=headers, json=json, impersonate=impersonate, **kwargs)
+    def post(self, url: str, json: Optional[Dict[str, Any]] = None, headers: Optional[Dict[str, str]] = None, **kwargs):
+        return self.session.post(str(url), headers=headers, json=json, **kwargs)
 
     def close(self):
         self.session.close()
 
 
+class ProxyProtocol(Enum):
+    HTTP = "http"
+    HTTPS = "https"
+    SOCKS4 = "socks4"
+    SOCKS5 = "socks5"
+
+
+class Proxy(BaseModel):
+    protocol: ProxyProtocol
+    url: str
+    username: Optional[str]
+    password: Optional[str]
+
+
+class Impersonate(Enum):
+    chrome99 = "chrome99"
+    chrome100 = "chrome100"
+    chrome101 = "chrome101"
+    chrome104 = "chrome104"
+    chrome107 = "chrome107"
+    chrome110 = "chrome110"
+    chrome99_android = "chrome99_android"
+    edge99 = "edge99"
+    edge101 = "edge101"
+    safari15_3 = "safari15_3"
+    safari15_5 = "safari15_5"
+
+
+class SessionCreateRequest(BaseModel):
+    impersonate: Impersonate
+    session: Optional[str] = None
+    proxy: Optional[Proxy] = None
+
+
 class SessionData:
     session: SimpleSession
-    cookies: Dict[str, Any]
-    proxy: Optional[Dict[str, Any]]
+    cookies: Optional[Dict[str, Any]]
+    proxy: Optional[Proxy]
+
+
+class SessionDestroyRequest(BaseModel):
+    session: str
+
+
+class HttpMethod(str, Enum):
+    GET = "GET"
+    POST = "POST"
+
+
+class Request(BaseModel):
+    method: HttpMethod
+    url: HttpUrl
+    session: str
+    headers: Optional[Dict[str, Any]] = None
+    postData: Optional[Dict[str, Any]] = None
+    returnOnlyCookies: Optional[bool] = False
 
 
 class cURLsolverrManager:
     def __init__(self):
         self.sessions = {}
 
-    def create_session(self, session_id: Optional[str] = None, proxy: Optional[Dict] = None) -> str:
+    def create_session(self, impersonate: Impersonate, session_id: Optional[str] = None, proxy: Optional[Proxy] = None) -> str:
         if not session_id:
             session_id = str(uuid.uuid4())
 
         simple_session = SimpleSession()
         if proxy:
-            simple_session.session.proxies = proxy
+            simple_session.session.proxies = self.__get_proxies(proxy)
+
+        if impersonate:
+            simple_session.session.impersonate = str(impersonate.value)
 
         self.sessions[session_id] = {
             "session": simple_session,
             "cookies": {},
             "proxy": proxy
         }
-        return session_id
+        return {"sessionId": session_id, "proxy": proxy}
 
     def list_sessions(self) -> List[str]:
-        return list(self.sessions.keys())
+        return self.sessions
 
     def destroy_session(self, session_id: str) -> None:
         if session_id in self.sessions:
@@ -62,7 +117,8 @@ class cURLsolverrManager:
         simple_session: SimpleSession = session_data["session"]
 
         try:
-            response = simple_session.get(url, headers=headers, ** kwargs)
+            response = simple_session.get(
+                url, headers=headers, ** kwargs)
         except Exception as e:  # Adjusted error handling
             return {"error": str(e)}
 
@@ -89,7 +145,7 @@ class cURLsolverrManager:
             response = simple_session.post(
                 url, json=data, headers=headers, **kwargs)
         except Exception as e:  # Adjusted error handling
-            return {"error": str(e)}
+            return {"PostError": str(e)}
 
         # Store the cookies from the response
         if session_id:
@@ -103,50 +159,32 @@ class cURLsolverrManager:
             "response": response.content
         }
 
+    def __get_proxies(self, proxy: Proxy) -> Optional[Dict[str, str]]:
+        if not proxy:
+            return None
+
+        proxy_url = str(proxy.url)
+        if proxy.username and proxy.password:
+            return {"https": f"{proxy.protocol.value}://{proxy.username}:{proxy.password}@{proxy_url}"}
+        return {"https": f"{proxy.protocol.value}://{proxy_url}"}
+
 
 app = FastAPI()
 
 curlManager = cURLsolverrManager()
 
 
-class Proxy(BaseModel):
-    url: HttpUrl
-    username: Optional[str]
-    password: Optional[str]
-
-
-class SessionCreateRequest(BaseModel):
-    session: Optional[str]
-    proxy: Optional[Proxy]
-
-
-class SessionDestroyRequest(BaseModel):
-    session: str
-
-
-class HttpMethod(str, Enum):
-    GET = "GET"
-    POST = "POST"
-
-
-class Request(BaseModel):
-    method: HttpMethod
-    url: HttpUrl
-    session: str
-    headers: Optional[Dict[str, Any]] = None
-    postData: Optional[Dict[str, Any]] = None
-    returnOnlyCookies: Optional[bool] = False
-
-
 @app.post("/v1/session/")
 def create_session(request: Optional[SessionCreateRequest] = None):
     session_id = None
     proxy = None
+    impersonate = None
     if request:
+        impersonate = request.impersonate
         session_id = request.session
         proxy = request.proxy
     session_id = curlManager.create_session(
-        session_id=session_id, proxy=proxy)
+        session_id=session_id, proxy=proxy, impersonate=impersonate)
     return {"session": session_id}
 
 
